224b8f6e6f2dcbdf8619e7c81c4b45bc11849e4f
Krzysztof Kepinski <phpworker@gmail.com>
2018-06-30 18:34:07 +0200

    cleanup

--- arrays.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// what will be the output?
-
-$a = array(1, 2, 3);
-$b = array(1 => 2, 2 => 3, 0 => 1);
-
-var_dump($a == $b);
--- strstr-returns.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// What will be the output?
-
-$h = '123456123456';
-$n = '123';
-echo strstr($h, $n);
